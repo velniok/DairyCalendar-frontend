b60b18c8a567704096f3e9ba1af98a9d6dbe0404
Ivan <mvzxw2@gmail.com>
2026-01-06 19:40:00 +0300

    1

--- src/axios.ts
+++ src/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: "https://dairy-calendar-backend--mvzxw2.replit.app/api",
 })
 
 instance.interceptors.request.use((config) => {
